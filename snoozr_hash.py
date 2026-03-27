4a8a03f987ebc027aa911c5fe7f0d76fbad82659
ignatiushewett <ignatius.hew@gmail.com>
2026-03-27 22:16:04 +1100

    Added account verification

--- snoozr_hash.py
+++ snoozr_hash.py
@@ -8,3 +8,11 @@ hashed = bcrypt.hashpw(password_bytes, salt)
 
 print(f"\nSalt:   {salt.decode('utf-8')}")
 print(f"Hash:   {hashed.decode('utf-8')}")
+
+attempt = input("\nRe-enter password to verify: ")
+attempt_bytes = attempt.encode('utf-8')
+
+if bcrypt.checkpw(attempt_bytes, hashed):
+    print("Correct")
+else:
+    print("Incorrect")
